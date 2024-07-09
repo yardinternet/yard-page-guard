df841f805190e4bdf6b14d43aea5d880d37d7801
Mike van den Hoek <mike@yard.nl>
2024-07-09 07:22:01 +0200

    (chore): add basis for testing

---
+++ tests/TestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yard\PageGuard\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnit;
+
+class TestCase extends PHPUnit
+{
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = [])
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Load dependencies with Composer autoloader.
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
+define('WP_PLUGIN_DIR', __DIR__);
+define('WP_DEBUG', false);
+
+/**
+ * Bootstrap WordPress Mock.
+ */
+\WP_Mock::setUsePatchwork(true);
+\WP_Mock::bootstrap();
+
+$GLOBALS['yard-blocks'] = [
+	'active_plugins' => ['yard-page-guard/yard-page-guard.php'],
+];
+
+class WP_CLI
+{
+	public static function add_command()
+	{
+	}
+}
+
+if (! function_exists('get_echo')) {
+	/**
+	 * Capture the echo of a callable function.
+	 *
+	 * @param       $callable
+	 * @param array $args
+	 *
+	 * @return string
+	 */
+	function get_echo($callable, $args = [])
+	{
+		ob_start();
+		call_user_func_array($callable, $args);
+
+		return ob_get_clean();
+	}
+}
