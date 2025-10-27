dfd4cd506d7fdc16d9f020340e5ff7f209cf5584
rivanuff <rivanuff@gmail.com>
2025-10-27 10:11:38 +0100

    chore: initialize branch, update deps, format

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const defaultConfig = require( '@wordpress/scripts/config/webpack.config' ); // Original config from the @wordpress/scripts package.
+const defaultConfig = require('@wordpress/scripts/config/webpack.config'); // Original config from the @wordpress/scripts package.
 
 module.exports = {
 	...defaultConfig,
 	entry: {
-		editor: [ './resources/scss/editor/style.scss' ],
+		editor: ['./resources/scss/editor/style.scss'],
 	},
 };
