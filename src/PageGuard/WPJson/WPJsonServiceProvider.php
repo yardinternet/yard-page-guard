df1fa2d3c9350426bfcb6797517031d811bb8a5e
rivanuff <rivanuff@gmail.com>
2025-11-04 10:52:16 +0100

    chore: implement htmx in form

--- src/PageGuard/WPJson/WPJsonServiceProvider.php
+++ src/PageGuard/WPJson/WPJsonServiceProvider.php
@@ -3,16 +3,19 @@
 namespace Yard\PageGuard\WPJson;
 
 use Yard\PageGuard\Foundation\ServiceProvider;
+use Yard\PageGuard\WPJson\Controllers\VerifyPostController;
 
 class WPJsonServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         add_action('rest_api_init', function () {
+            $verifyPostController = new VerifyPostController();
+
             register_rest_route('yard-page-guard/v1', '/verify-post', [
                 'methods' => 'POST',
-                'callback' => [Controllers\VerifyPostController::class, 'handleRequest'],
-                'args' => Controllers\VerifyPostController::getEndpointArgs(),
+                'callback' => [$verifyPostController, 'handleRequest'],
+                'args' => $verifyPostController->getEndpointArgs(),
                 'permission_callback' => '__return_true',
             ]);
         });
