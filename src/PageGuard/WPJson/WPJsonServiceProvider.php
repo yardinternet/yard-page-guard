429a8ecab08b6eb7adf63e3f97ca077bb1f4a14c
rivanuff <rivanuff@gmail.com>
2025-11-19 11:10:14 +0100

    feat: finish external (fusion pdc/pub) verification

--- src/PageGuard/WPJson/WPJsonServiceProvider.php
+++ src/PageGuard/WPJson/WPJsonServiceProvider.php
@@ -2,12 +2,17 @@
 
 namespace Yard\PageGuard\WPJson;
 
+use WP_Error;
+use WP_REST_Request;
 use Yard\PageGuard\Foundation\ServiceProvider;
+use Yard\PageGuard\Traits\Token;
 use Yard\PageGuard\WPJson\Controllers\ModalInfoController;
 use Yard\PageGuard\WPJson\Controllers\VerifyPostController;
 
 class WPJsonServiceProvider extends ServiceProvider
 {
+    use Token;
+
     public function register(): void
     {
         add_action('rest_api_init', function () {
@@ -17,14 +22,14 @@ class WPJsonServiceProvider extends ServiceProvider
             register_rest_route('yard-page-guard/v1', '/verify-post', [
                 'methods' => 'POST',
                 'callback' => [$verifyPostController, 'handleRequest'],
-                'args' => VerifyPostController::getEndpointArgs(),
+                'args' => self::getEndpointArgs(),
                 'permission_callback' => '__return_true',
             ]);
 
             register_rest_route('yard-page-guard/v1', '/modal-info', [
                 'methods' => 'POST',
                 'callback' => [$modalInfoController, 'handleRequest'],
-                'args' => ModalInfoController::getEndpointArgs(),
+                'args' => self::getEndpointArgs(),
                 'permission_callback' => '__return_true',
             ]);
 
@@ -38,5 +43,40 @@ class WPJsonServiceProvider extends ServiceProvider
                 return $served;
             });
         });
+    }
+
+    public static function getEndpointArgs(): array
+    {
+        return [
+            'post_id' => [
+                'required' => true,
+                'type' => 'integer',
+                'validate_callback' => function (int $postId) {
+                    if (get_post_status($postId) === false) {
+                        return new WP_Error(
+                            'invalid_post_id',
+                            __('Ongeldige post ID', 'yard-page-guard')
+                        );
+                    }
+
+                    return true;
+                },
+            ],
+            'ypg_review_token' => [
+                'required' => true,
+                'type' => 'string',
+                'validate_callback' => function (string $reviewToken, WP_REST_Request $request): bool {
+                    $postId = (int) $request->get_param('post_id');
+                    $contentOwnerEmail = get_post_meta($postId, 'ypg_post_content_owner_email', true) ?? '';
+                    $reviewDate = get_post_meta($postId, 'ypg_review_date', true) ?? '';
+
+                    if ('' === $contentOwnerEmail || '' === $reviewDate) {
+                        return false;
+                    }
+
+                    return self::verifyReviewToken($postId, $contentOwnerEmail, $reviewDate, $reviewToken);
+                },
+            ],
+        ];
     }
 }
