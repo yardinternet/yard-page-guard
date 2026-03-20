7ce87f4bde81b9132d4323af89cf8c0ca403c724
rivanuff <rivanuff@gmail.com>
2026-03-20 14:37:16 +0100

    chore: make admin roles filterable

--- yard-page-guard.php
+++ yard-page-guard.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * Author URI:      http://www.yard.nl
  * Text Domain:     yard-page-guard
  * Domain Path:     /languages
- * Version:         2.0.0
+ * Version:         2.0.1
  *
  * @package         Yard_Page_Guard
  */
@@ -32,7 +32,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 	$autoloader = new Yard\PageGuard\Autoloader();
 }
 
-define('YPG_VERSION', '2.0.0');
+define('YPG_VERSION', '2.0.1');
 define('YPG_PLUGIN_NAME', basename(__DIR__));
 
 /**
