00e060beb8bd416aeda2c5b6f50d363d0bce770f
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2026-07-28 14:50:42 +0200

    feat: use native WP List table

--- src/PageGuard/Admin/Controllers/AdminOverviewController.php
+++ src/PageGuard/Admin/Controllers/AdminOverviewController.php
@@ -11,7 +11,7 @@ if (! defined('ABSPATH')) {
 	exit;
 }
 
-use Yard\PageGuard\Admin\Services\AdminOverviewService;
+use Yard\PageGuard\Admin\ListTables\PageGuardListTable;
 use Yard\PageGuard\Traits\Date;
 use Yard\PageGuard\Traits\Text;
 
@@ -20,23 +20,21 @@ class AdminOverviewController
 	use Text;
 	use Date;
 
-	private AdminOverviewService $service;
-
-	public function __construct()
-	{
-		$this->service = new AdminOverviewService();
-	}
-
 	public function init(): void
 	{
 		add_action('admin_menu', [$this, 'addOverviewPage']);
 		add_action('admin_menu', [$this, 'addOverviewSubPage']);
-		add_action('admin_post_ypg_bulk_update', [$this->service, 'handleBulkEdit']);
+		add_filter('set_screen_option_toplevel_page_ypg_overview_per_page', [$this, 'setScreenOptionPerPage'], 10, 3);
+	}
+
+	public function setScreenOptionPerPage($screenOption, string $option, $value)
+	{
+		return (int) $value;
 	}
 
 	public function addOverviewPage(): void
 	{
-		add_menu_page(
+		$adminScreen = add_menu_page(
 			__('Houdbaarheids Overzicht', 'yard-page-guard'),
 			__('Houdbaarheids Overzicht', 'yard-page-guard'),
 			apply_filters('yard::page-guard/capability/admin', 'edit_pages'),
@@ -45,6 +43,13 @@ class AdminOverviewController
 			'dashicons-visibility',
 			20
 		);
+
+		add_action("load-{$adminScreen}", [$this, 'addScreenOptionPerPage']);
+	}
+
+	public function addScreenOptionPerPage(): void
+	{
+		add_screen_option('per_page');
 	}
 
 	public function addOverviewSubPage(): void
@@ -60,7 +65,20 @@ class AdminOverviewController
 
 	public function renderOverviewPage(): void
 	{
-		require_once __DIR__ . '/../Views/AdminOverviewPage.php';
+		$listTable = new PageGuardListTable([]);
+		$listTable->process_bulk_action();
+		$listTable->prepare_items(); //
+		settings_errors();
+		?>
+		<div class="wrap">
+			<h1 class="wp-heading-inline"><?php echo esc_html(get_admin_page_title()); ?></h1>
+			<hr class="wp-header-end">
+			<?php $listTable->views();?>
+			<form id="yard-page-guard-table-form" method="post">
+				<?php $listTable->display();?>
+			</form>
+		</div>
+		<?php
 	}
 
 	public function redirectToExternalContentOwners(): void
