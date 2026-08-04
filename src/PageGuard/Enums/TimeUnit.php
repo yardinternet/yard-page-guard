a1ed4afc50c30e38bed9ecdd0efa47e9d2026218
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2026-08-04 13:20:21 +0200

    chore: add timeunit enum

---
+++ src/PageGuard/Enums/TimeUnit.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Yard\PageGuard\Enums;
+
+class TimeUnit
+{
+	public const DAYS = 'days';
+	public const WEEKS = 'weeks';
+	public const MONTHS = 'months';
+
+	public static function options(): array
+	{
+		return [
+			self::DAYS => __('Dagen', 'yard-page-guard'),
+			self::WEEKS => __('Weken', 'yard-page-guard'),
+			self::MONTHS => __('Maanden', 'yard-page-guard'),
+		];
+	}
+}
