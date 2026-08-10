214d10edd0e258b072d89dbc97c2e5ca5488ce5e
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2026-08-10 14:43:47 +0200

    chore: default language is dutch

--- src/PageGuard/Traits/Date.php
+++ src/PageGuard/Traits/Date.php
@@ -12,12 +12,12 @@ trait Date
 	{
 		switch ($unit) {
 			case TimeUnit::WEEKS:
-				return sprintf(_n('%d week', '%d weeks', $period, 'yard-page-guard'), $period);
+				return sprintf(_n('%d week', '%d weken', $period, 'yard-page-guard'), $period);
 			case TimeUnit::MONTHS:
-				return sprintf(_n('%d month', '%d months', $period, 'yard-page-guard'), $period);
+				return sprintf(_n('%d maand', '%d maanden', $period, 'yard-page-guard'), $period);
 			case TimeUnit::DAYS:
 			default:
-				return sprintf(_n('%d day', '%d days', $period, 'yard-page-guard'), $period);
+				return sprintf(_n('%d dag', '%d dagen', $period, 'yard-page-guard'), $period);
 		}
 	}
 }
