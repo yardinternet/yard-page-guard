7e39740459cc502438475e1f950f2fff4ece2eeb
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2026-08-20 19:06:34 +0200

    fix: (term)meta defaults back to empty string
    null as default gave an error

--- src/PageGuard/Meta/TermMeta.php
+++ src/PageGuard/Meta/TermMeta.php
@@ -17,12 +17,12 @@ class TermMeta
 			self::EXTERNAL_CONTENT_OWNER_PHONE_NUMBER => [
 				'type' => 'string',
 				'sanitize_callback' => 'sanitize_text_field',
-				'default' => null,
+				'default' => '',
 			],
 			self::EXTERNAL_CONTENT_OWNER_EMAIL => [
 				'type' => 'string',
 				'sanitize_callback' => 'sanitize_email',
-				'default' => null,
+				'default' => '',
 			],
 		];
 
